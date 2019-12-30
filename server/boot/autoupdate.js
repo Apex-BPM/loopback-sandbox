0c940cfb67d1ffb34ff853b4b2921e8774d217f1
David Knapp <david.knapp@apexbpm.com>
2019-12-30 05:08:25 -0600

    Adjusted datasource settings and added logging of the settings

--- server/boot/autoupdate.js
+++ server/boot/autoupdate.js
@@ -2,6 +2,7 @@
 
 module.exports = function(app) {
   app.on('started', ()=>{
+    console.log('settings',app.dataSources.postgres.connector.settings);
     app.dataSources.postgres.connector.autoupdate((err)=>{
       if (err) return console.error('autoupdate err', err);
       console.log('autoupdate complete');
